c8857fe3406935e3e40c32343645f5707bbd876c
berlinguyinca <berlinguyinva@gmail.com>
2024-05-21 14:31:56 -0700

    Enhance Swap functions and add ParseTransaction in PumpFun
    Enhanced the SwapSolForToken and SwapTokenForSol functions in PumpFun to return detailed SwapResponse. Added a new function ParseTransaction to parse the transaction from the swap functions. Also, new tests for these functionalities have been implemented in pumpfun_test.go.

--- portal/exchange/pumpfun.go
+++ portal/exchange/pumpfun.go
@@ -2,12 +2,19 @@ package exchange
 
 import (
 	"bytes"
+	"context"
 	"encoding/json"
 	"errors"
 	"fmt"
+	"github.com/gagliardetto/solana-go"
+	"github.com/gagliardetto/solana-go/rpc"
 	"github.com/rs/zerolog/log"
+	"golang.org/x/time/rate"
 	"io/ioutil"
+	"math"
 	"net/http"
+	"strconv"
+	"time"
 )
 
 type PumpFun struct {
@@ -30,9 +37,11 @@ type SwapRequest struct {
 type SwapResponse struct {
 	Signature string `json:"signature"`
 	Errors    []any  `json:"errors"`
+	Request   SwapRequest
+	Filled    float64
 }
 
-func (p *PumpFun) SwapSolForToken(amountSol float64, token string, slippage float64, priorityFee float64) (any, error) {
+func (p *PumpFun) SwapSolForToken(amountSol float64, token string, slippage float64, priorityFee float64) (*SwapResponse, error) {
 
 	request := SwapRequest{
 		Action:           "buy",
@@ -47,7 +56,7 @@ func (p *PumpFun) SwapSolForToken(amountSol float64, token string, slippage floa
 	return p.SwapRequest(request)
 }
 
-func (p *PumpFun) SwapRequest(request SwapRequest) (any, error) {
+func (p *PumpFun) SwapRequest(request SwapRequest) (*SwapResponse, error) {
 	/*
 		if request.Amount <= 0 {
 			return nil, errors.New("invalid swap request, need to specify an amount larger than 0")
@@ -103,11 +112,101 @@ func (p *PumpFun) SwapRequest(request SwapRequest) (any, error) {
 
 		log.Info().Str("state", resp.Status).Any("response", sr).Msg("Request response")
 
-		return nil, nil
+		sr.Request = request
+		if len(sr.Errors) > 0 {
+			return &sr, errors.New("we received an error during this response")
+		}
+
+		filled, err := p.ParseTransaction(&sr)
+
+		if err != nil {
+			return &sr, err
+		}
+		sr.Filled = filled
+		return &sr, nil
 	}
 }
 
-func (p *PumpFun) SwapTokenForSol(amountToken float64, token string, slippage float64, priorityFee float64) (any, error) {
+func (p *PumpFun) ParseTransaction(sr *SwapResponse) (float64, error) {
+	client := rpc.NewWithCustomRPCClient(rpc.NewWithLimiter(
+		"https://alpha-quick-energy.solana-mainnet.quiknode.pro/a9a59fa0f479c337641331fadd97fb1239fd4744/",
+		rate.Every(time.Second), // time frame
+		5,                       // limit of requests per time frame
+	))
+
+	var v uint64 = 1
+
+	opts := rpc.GetParsedTransactionOpts{
+		MaxSupportedTransactionVersion: &v,
+	}
+	sig, err := solana.SignatureFromBase58(sr.Signature)
+	if err != nil {
+		return 0, err
+	}
+
+	var t *rpc.GetParsedTransactionResult
+	for i := 0; ; i++ {
+		t, err = client.GetParsedTransaction(context.TODO(), sig, &opts)
+
+		if err == nil {
+			//done
+			log.Info().Any("tx", t)
+			break
+		} else if err.Error() == "not found" {
+			log.Debug().AnErr("error", err).Msgf("received error, sleeping a bit...")
+			time.Sleep(1 * time.Second)
+		} else {
+			log.Error().AnErr("error", err)
+			return 0, err
+		}
+
+	}
+	log.Info().Any("tx", t).Msgf("fetched transactions")
+
+	if t.Meta.Err != nil {
+		log.Error().Str("pair", sr.Signature).Any("error in transaction", t.Meta.Err).Msgf("transaction most likely failed")
+		return 0, err
+	}
+
+	if sr.Request.Action == "buy" {
+		tb := p.findTokens(sr.Request.Mint, t.Meta.PreTokenBalances)
+		ta := p.findTokens(sr.Request.Mint, t.Meta.PostTokenBalances)
+		tokensBefore, err1 := strconv.ParseFloat(tb.Amount, 64)
+		tokensAfter, err2 := strconv.ParseFloat(ta.Amount, 64)
+
+		if err1 != nil {
+			return 0, err1
+		}
+		if err2 != nil {
+			return 0, err2
+		}
+		difference := tokensBefore - tokensAfter
+		adjusted := difference / math.Pow(10, float64(tb.Decimals))
+
+		log.Info().Float64("gained", difference).Msgf("we received an amount")
+
+		return adjusted, nil
+	} else {
+		solBefore := float64(t.Meta.PreBalances[0]) / 1000000000
+		solAfter := float64(t.Meta.PostBalances[0]) / 1000000000
+		gain := solAfter - solBefore
+
+		return gain, nil
+
+	}
+}
+
+func (p *PumpFun) findTokens(mint string, balance []rpc.TokenBalance) *rpc.UiTokenAmount {
+	for _, token := range balance {
+		if token.Mint.String() == mint {
+			return token.UiTokenAmount
+		}
+	}
+
+	return nil
+}
+
+func (p *PumpFun) SwapTokenForSol(amountToken float64, token string, slippage float64, priorityFee float64) (*SwapResponse, error) {
 
 	request := SwapRequest{
 		Action:           "sell",
--- portal/exchange/pumpfun_test.go
+++ portal/exchange/pumpfun_test.go
@@ -2,6 +2,7 @@ package exchange
 
 import (
 	"github.com/joho/godotenv"
+	log2 "github.com/rs/zerolog/log"
 	"github.com/test-go/testify/assert"
 	"net/http"
 	"os"
@@ -34,9 +35,27 @@ func TestPumpFun_SwapSolForTokenWithApiKeyAndValidToken(t *testing.T) {
 
 	p := NewPumpFun(os.Getenv("PUMPFUN_API_KEY"), &http.Client{})
 
-	_, err = p.SwapSolForToken(0.05, "4qQoezLr1WGzcoTiM4w9xSasB77UNshkE9ifGzWbe5Px", 0, 0.01)
+	sr, err := p.SwapSolForToken(0.005, "CotwVjzJnjhUEYw3xFLuhVw1B3xcfdJDEkNYnfcaf2Bb", 0.5, 0.01)
 
 	assert.NoError(t, err)
+
+	sr2, err2 := p.SwapTokenForSol(sr.Filled, "CotwVjzJnjhUEYw3xFLuhVw1B3xcfdJDEkNYnfcaf2Bb", 0.5, 0.01)
+
+	assert.NoError(t, err2)
+
+	log2.Info().Any("response", sr2).Msgf("test finished")
+
+}
+
+func TestPumpFun_SwapSolForTokenWithApiKeyAndValidTokenFailsDueToSimulationError(t *testing.T) {
+	err := godotenv.Load("../../.env")
+	assert.NoError(t, err)
+
+	p := NewPumpFun(os.Getenv("PUMPFUN_API_KEY"), &http.Client{})
+
+	_, err = p.SwapSolForToken(0.005, "8ReVeUanKktF6mAhCcf3JgCseAWjbo1rqUeNqwymuAQ1", 0.5, 0.01)
+
+	assert.Error(t, err)
 }
 
 func TestPumpFun_SwapSolForTokenWithApiKeyAndEmptyToken(t *testing.T) {
